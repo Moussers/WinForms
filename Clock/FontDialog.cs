02bf61b8457c541d04f9d252a6b7f2f0d0d14eaa
sandCat <Sand@DESKTOP-K3SU1F7>
2025-12-26 21:15:50 +0300

    FontFialog DONE

--- Clock/FontDialog.cs
+++ Clock/FontDialog.cs
@@ -22,10 +22,12 @@ namespace Clock
             lastChosenIndex = 0;
             LoadFonts("*.ttf");
             LoadFonts("*.otf");
+            comboBoxFont.SelectedIndex = 0;
         }
 
         private void FontDialog_Load(object sender, EventArgs e)
         {
+            numericUpDownFontSize.Value = (decimal)Font.Size;
         }
         void LoadFonts(string extension) 
         {
@@ -55,13 +57,13 @@ namespace Clock
             info += $"\nitem:\t{comboBoxFont.SelectedText}";
             info += $"\nitem:\t{comboBoxFont.SelectedValue}";
             //MessageBox.Show(this, info, "SelectedIndexChanged", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            SetFont(comboBoxFont.SelectedItem.ToString());
+            SetFont();
         }
-        void SetFont(string filename)
+        void SetFont()
         {
             PrivateFontCollection pfc = new PrivateFontCollection();
-            pfc.AddFontFile(filename);
-            labelExample.Font = new Font(pfc.Families[0], 32);
+            pfc.AddFontFile(comboBoxFont.SelectedItem.ToString());
+            labelExample.Font = new Font(pfc.Families[0], (float)numericUpDownFontSize.Value);
         }
 
         private void buttonOK_Click(object sender, EventArgs e)
@@ -74,6 +76,11 @@ namespace Clock
         {
             labelExample.Font = this.Font;
             comboBoxFont.SelectedIndex = lastChosenIndex;
+        }
+
+        private void numericUpDownFontSize_ValueChanged(object sender, EventArgs e)
+        {
+            SetFont();
         }
     }
 }
