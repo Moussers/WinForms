a700a87bebbec73d63f7dcfadcfe6d938659c843
sandCat <Sand@DESKTOP-K3SU1F7>
2025-12-29 22:35:11 +0300

    Fixing problem, when the file.ini open by yourself

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -68,7 +68,7 @@ namespace Clock
 
             writer.Close();
 
-            System.Diagnostics.Process.Start("notepad", "Settings.ini");
+           //System.Diagnostics.Process.Start("notepad", "Settings.ini");
         }
         void LoadSettings()
         {
